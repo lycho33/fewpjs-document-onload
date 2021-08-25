12c5d5111f5fb4293b25f755d57ee27e840a5923
lycho33 <cho.lyds@gmail.com>
2021-08-24 22:46:36 -0400

    commit tree 824d1a2071ae47dc60bc0f7017c6abb8a4418208

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+//1. DOM Content loads the HTML page first
+//2. Then everything else is loaded
+//you can add the function inside here so the HTML will show once the DOM is loaded 
+document.addEventListener("DOMContentLoaded", function() {
+  updateDOM()
+});
+
+function updateDOM(){
+  let p = document.querySelector('p')
+  p.innerText = 'This is really cool!'
+}
